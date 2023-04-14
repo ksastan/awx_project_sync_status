f1a1078b25b9af182d09f8fbf42b2e23b09d29eb
stanislav.kuchinsky <stanislav.kuchinsky@x5.ru>
2023-04-14 17:47:01 +0300

    add script and readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # awx_project_sync_status
+Script checks AWX project synchronisation status and provide it as prometheus metric.
+Metrics endpoint `/metrics`, e.g.:
+```shell
+htttp://<ip>:<METRICS_PORT>/metrics
+```
+
+# Prerequisites
+Install python modules
+```shell
+pip install requirements.txt
+```
+
+# Environment variables
+- METRICS_PORT - listening TCP port, e.g. 8000
+- AWX_URL - AWX URL address, http://awx.server/
+- AWX_TOKEN - token to access AWX
+- CHECKS_DELAY - delay between projects checks
+
+Variables can be exported in shell or in other ways, e.g. docker via docker/k8s envs:
+```shell
+export METRICS_PORT="8000"
+export AWX_URL="https://my.awx/"
+export AWX_TOKEN="my-secret-token"
+```
---
+++ main.py
@@ -0,0 +1,42 @@
+import requests
+import sys
+import time
+import os
+from prometheus_client import start_http_server, Gauge
+
+METRICS_PORT = os.environ.get('METRICS_PORT', 16784)
+AWX_URL = os.environ.get('AWX_URL')
+AWX_TOKEN = os.environ.get('AWX_TOKEN')
+FAILED_STATUSES = {"failed", "error", "canceled"}
+CHECKS_DELAY = os.environ.get('CHECKS_DELAY', 10)
+# todo: replace by environment variable
+PROJECTS_ID_TO_CHECK = [] # awx projects ID
+
+
+def get_project_info(project_id, session):
+    project_info = session.get(f"{AWX_URL}/api/v2/projects/{project_id}", headers={'Authorization': f'Bearer {AWX_TOKEN}'})
+    if project_info.status_code < 400:
+        return project_info.json()
+    else:
+        print(f"AWX connection error http_status={project_info.status_code}")
+        sys.exit(1)
+
+
+def main():
+    start_http_server(METRICS_PORT)
+
+    # add metrics
+    projects_status = Gauge('awx_project_up', 'Status of AWX project synchronisation', ['project_id', 'project_name'])
+
+    # check endpoints every 'CHECKS_DELAY'
+    while True:
+        with requests.Session() as s:
+            for project in PROJECTS_ID_TO_CHECK:
+                project_info = get_project_info(project, s)
+                projects_status.labels(project_id=project).set(
+                    1 if project_info['status'] not in FAILED_STATUSES else 0)
+        time.sleep(CHECKS_DELAY)
+
+
+if __name__ == '__main__':
+    main()
